694dd58c9591a1b914c21f00f00fb591e9270a5a
hoshizaki <kant.cmd@gmail.com>
2023-03-24 18:03:16 +0700

    fix typo

--- src/config/config.js
+++ src/config/config.js
@@ -9,7 +9,7 @@ class Config {
     constructor() {
         this.port = parseInt(process.env.PORT, 10) || 8080;
         this.env = process.env.ENV || 'dev';
-        const host = process.env.HOST || '127.0.0.1';
+        const host = process.env.MONGO_HOST || '127.0.0.1';
         const dbPort = process.env.MONGO_PORT || '27017';
 
         this.dbHost = `${host}:${dbPort}`
