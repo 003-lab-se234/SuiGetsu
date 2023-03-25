1f503ec97dbf4ae4aa79e8f9d7e6a3608e7ff1d6
hoshizaki <kant.cmd@gmail.com>
2023-03-26 00:50:15 +0700

    add static endpoint

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const path = require('path');
 const { log } = require('../middleware');
 
 router.get('/', log, (req, res) => {
@@ -8,5 +9,30 @@ router.get('/', log, (req, res) => {
         title: "Hello Human"
     })
 })
+
+router.get('/about', (req, res) => {
+    // res.send("To be continued");
+    res.status(200);
+    res.setHeader('content-type', 'text/html');
+    res.sendFile(path.join(__dirname, '..', '..', 'public', 'about.html'))
+
+})
+
+router.get('/contact', (req, res) => {
+    // res.send("To be continued");
+    res.status(200);
+    res.setHeader('content-type', 'text/html');
+    res.sendFile(path.join(__dirname, '..', '..', 'public', 'contact.html'));
+})
+
+router.get('/menu', (req, res) => {
+
+    // check wether user have logged in or not
+    // res.render('menu.ejs' , { user } ) if user is logged in order and cart button will be displayed
+    res.status(200);
+    res.setHeader("Content-type", "text/html");
+    res.send("To be continued");
+})
+
 
 module.exports = router;
