41664494ebe1dadc893a39fcc0ae26a803692bb0
hoshizaki <kant.cmd@gmail.com>
2023-03-29 01:36:57 +0700

    hot-fix menu endpoint

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,8 +4,10 @@ const config = require('../config/config');
 const authController = require('../controllers/auth.controller');
 const { log } = require('../middleware');
 const { authentication, secureRoute } = require('../middleware/auth');
+const { Food } = require('../models/Food');
 const { User } = require('../models/User');
 const logger = require('../utilities/logger');
+const { paginate } = require('../utilities/paginate');
 const staffRouter = require('./staff.router');
 
 router.use(log)
@@ -42,10 +44,10 @@ router.get('/about', async (req, res) => {
 
 })
 
-router.get('/contact', async(req, res) => {
-    let user = {} ;
+router.get('/contact', async (req, res) => {
+    let user = {};
     try {
-        const id = req.session.userId ;
+        const id = req.session.userId;
         user = await User.findById(id);
         // console.log(user)
     } catch (err) {
@@ -53,17 +55,35 @@ router.get('/contact', async(req, res) => {
     }
     res.status(200);
     res.setHeader('content-type', 'text/html');
-    res.render('publicPages/contact.ejs', {user});
+    res.render('publicPages/contact.ejs', { user });
     // res.sendFile(path.join(__dirname, '..', '..', 'public','html', 'contact.html'));
 })
 
-router.get('/menu', (req, res) => {
-    res.status(200);
-    res.setHeader("Content-type", "text/html");
-    res.send("To be continued");
+router.get('/menu', async (req, res) => {
+    try {
+        let user = {};
+        const id = req.session.userId;
+        user = await User.findById(id);
+
+        let { category, page, title } = req.query;
+        let filter = {}
+        if (!page) page = 1;
+        if (category && category != "any") filter = { ...filter, category };
+        if (title) filter = { ...filter, title: { $regex: new RegExp(`${title}`), $options: 'i' } };
+        let sort = { is_outofstock: 1, updatedAt: 1 }
+        const foods = await Food.find(filter).sort(sort);
+        const paginateFood = paginate(foods, page, 6);
+        res.render('publicPages/menus.ejs', { user, data: paginateFood, category, page, title });
+
+
+    } catch (err) {
+        logger.error(err);
+        res.status(500);
+        return res.render('publicPages/error.ejs', { error: err })
+    }
 })
 
-router.use('/staff', secureRoute ,staffRouter);
+router.use('/staff', secureRoute, staffRouter);
 // router.use('/user', authentication , userRouter);
 router.use('/auth', authController);
 
